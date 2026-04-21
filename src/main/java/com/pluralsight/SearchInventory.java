4a2c66812d1168dbab5dd3b8356287c2f6f73e01
Moubarak Congacou <cmouba1999@gmail.com>
2026-04-21 18:17:46 -0400

    Loaded the ArrayList and displayed it

--- src/main/java/com/pluralsight/SearchInventory.java
+++ src/main/java/com/pluralsight/SearchInventory.java
@@ -1,31 +1,40 @@
 package com.pluralsight;
 import java.util.ArrayList;
-import java.util.Scanner;
+
 
 
 public class SearchInventory {
     public static void main(String[] args) {
 
-        ArrayList<String> products = new ArrayList<String>();
+       //Get Inventory
+        ArrayList<Product> inventory = getInventory();
 
-        products.add("Sugar");
-        products.add("Biscuit");
-        products.add("Water");
-        products.add("TV");
-        products.add("Computer");
-        products.add("Books");
 
-//        for (int i = 0; i < products.size(); i++){
-//
-//            System.out.println();
+        //Display inventory
+        for (int i = 0; i < inventory.size(); i++){
+            Product p = inventory.get(i);
+            System.out.println((i + 1) + " : " + p.getName() + " - $" + p.getPrice());
 
         }
 
+    }
+
+    public static ArrayList<Product> getInventory(){
+
+        //ArrayList of products
+        ArrayList<Product> products = new ArrayList<>();
 
 
+        //Adding products to the list
+        products.add(new Product(1, "Sugar", 3.99));
+        products.add(new Product(2, "Biscuit", 5.99));
+        products.add(new Product(3, "Water", 2.50));
+        products.add(new Product(4, "TV", 89.99));
+        products.add(new Product(5, "Computer", 450.99));
+        products.add(new Product(6, "Book", 40.99));
+        products.add(new Product(7, "T-shirt", 30.99));
 
-
-
+        return products;
 
 
 
