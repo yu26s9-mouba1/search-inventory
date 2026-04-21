b685ae79335d0958a447c87084a42304c15e0804
Moubarak Congacou <cmouba1999@gmail.com>
2026-04-21 17:19:05 -0400

    Created an arrayList of 6 products

--- src/main/java/com/pluralsight/SearchInventory.java
+++ src/main/java/com/pluralsight/SearchInventory.java
@@ -1,7 +1,33 @@
 package com.pluralsight;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class SearchInventory {
     public static void main(String[] args) {
-        
+
+        ArrayList<String> products = new ArrayList<String>();
+
+        products.add("Sugar");
+        products.add("Biscuit");
+        products.add("Water");
+        products.add("TV");
+        products.add("Computer");
+        products.add("Books");
+
+//        for (int i = 0; i < products.size(); i++){
+//
+//            System.out.println();
+
+        }
+
+
+
+
+
+
+
+
+
     }
 }
